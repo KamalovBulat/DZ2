2dcfe8a19497dba140916757262679dd9409c5a9
KamalovBulat <75981496+KamalovBulat@users.noreply.github.com>
2021-06-21 01:25:20 +0300

    all files

---
+++ functions.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void console_set_text_color(int text_color, int background_color);
+
+void console_gotoxy(int column, int line);
+
+void console_hide_cursor();
+
+WORD console_get_vk();
+
+
+#endif
